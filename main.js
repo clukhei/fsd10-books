66cf4a3512ed8b8fa79d325adb3b1ab6e75cb806
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-06 10:22:32 +0800

    task 4 WIP
    display books with prev and next button working

--- main.js
+++ main.js
@@ -19,6 +19,40 @@ app.use(express.static(__dirname + "/static"))
 app.engine("hbs", hbs({defaultLayout: "default.hbs"}))
 app.set("view engine", "hbs")
 
+//SQL
+const SQL_GET_BOOK_LIST = 'SELECT * FROM goodreads.book2018 WHERE title LIKE ? order by title asc limit ? offset ? '
+const SQL_COUNT_GET_BOOK_LIST = 'SELECT count(*) as count FROM goodreads.book2018 WHERE title LIKE ?'
+
+app.get("/:alphaNum", async(req,res)=> {
+    const letter = req.params.alphaNum
+    const offset = parseInt(req.query['offset']) || 0
+    const limit = 10
+    const conn = await pool.getConnection()
+    try{
+        const result = await conn.query(SQL_GET_BOOK_LIST,[`${letter}%`,limit, offset])
+        const records = result[0]
+        console.log(records)
+        res.status(200)
+        res.type('text/html')
+        res.render('list', {
+            letter,
+            hasRecords: records.length > 0,
+            records,
+            prevOffset: Math.max(0, offset - limit) ,
+            nextOffset: offset + limit
+        })
+
+       
+    } catch(e) {
+        res.status(500)
+        res.type('text/html')
+        res.send(JSON.stringify(e))
+    } finally {
+        conn.release()
+    }
+
+})
+
 app.get("/", (req,res)=> {
     const alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('')
     console.log(alphaNum)
@@ -26,6 +60,11 @@ app.get("/", (req,res)=> {
     res.type('text/html')
     res.render('index', {alphaNum})
 })
+
+
+app.use((req, res) => {
+	res.redirect("/");
+});
 const startApp = async(app, pool) => {
     try{
         const conn = await pool.getConnection()
